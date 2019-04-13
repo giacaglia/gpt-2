2fc1f76bf0aab7b8a665ab006ea450638bd72b01
Giuliano Giacaglia <raulzito234@gmail.com>
2019-04-13 13:21:48 -0700

    Change to a new name

--- tensorflow/interactive_conditional_samples.py
+++ tensorflow/interactive_conditional_samples.py
@@ -8,6 +8,58 @@ import tensorflow as tf
 
 import model, sample, encoder
 
+def prediction_next_word(
+    model_name='117M',
+    seed=None,
+    nsamples=1,
+    batch_size=1,
+    length=2,
+    temperature=1,
+    top_k=40,
+    sentence="Hello, how are"):
+    if batch_size is None:
+        batch_size = 1
+    assert nsamples % batch_size == 0
+
+    enc = encoder.get_encoder(model_name)
+    hparams = model.default_hparams()
+    with open(os.path.join('models', model_name, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+
+    if length is None:
+        length = hparams.n_ctx // 2
+    elif length > hparams.n_ctx:
+        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+
+    with tf.Session(graph=tf.Graph()) as sess:
+        context = tf.placeholder(tf.int32, [batch_size, None])
+        np.random.seed(seed)
+        tf.set_random_seed(seed)
+        output = sample.sample_sequence(
+            hparams=hparams, length=length,
+            context=context,
+            batch_size=batch_size,
+            temperature=temperature, top_k=top_k
+        )
+
+        saver = tf.train.Saver()
+        ckpt = tf.train.latest_checkpoint(os.path.join('models', model_name))
+        saver.restore(sess, ckpt)
+
+        context_tokens = enc.encode(sentence)
+        generated = 0
+        response = ""
+        for _ in range(nsamples // batch_size):
+            out = sess.run(output, feed_dict={
+                context: [context_tokens for _ in range(batch_size)]
+            })[:, len(context_tokens):]
+            for i in range(batch_size):
+                generated += 1
+                text = enc.decode(out[i])
+                response += text
+
+    return response
+
 def interact_model(
     model_name='117M',
     seed=None,
@@ -82,6 +134,8 @@ def interact_model(
                     print(text)
             print("=" * 80)
 
-if __name__ == '__main__':
-    fire.Fire(interact_model)
+# if __name__ == '__main__':
+#     fire.Fire(interact_model)
 
+if __name__ == '__main__':
+    fire.Fire(prediction_next_word)
